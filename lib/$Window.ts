8221c2977a70817be12ba9376bb0fa3deec8ca7f
defaultkavy <defaultkavy@gmail.com>
2024-12-09 14:47:56 +0800

    delete

--- lib/$Window.ts
+++
@@ -1,9 +0,0 @@
-import { $EventManager, $EventMap } from "./$EventManager";
-import { $EventTarget } from "./$EventTarget";
-
-export class $Window<EM extends $WindowEventMap = $WindowEventMap> extends $EventTarget<EM, WindowEventMap> {
-    static $ = new $Window();
-    readonly dom = window;
-}
-
-export interface $WindowEventMap extends $EventMap {}
